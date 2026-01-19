bc8d16de6854ef00936b6987f24e612c3ae18de4
CHEBBI Talel <tchebbi.ext@MacBook-Pro.local>
2026-01-19 22:03:55 +0100

    Execute

--- infra/env/dev/outputs.tf
+++ infra/env/dev/outputs.tf
@@ -6,6 +6,7 @@ output "lambda_events_role_arn" {
   value = module.iam.lambda_events_role_arn
 }
 
+
 output "lambda_step_functions_role_arn" {
   value = module.iam.lambda_step_functions_role_arn
 }
