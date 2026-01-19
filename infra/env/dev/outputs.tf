6ce2000889bed563ed50af08ee0bce0c3d3c24a3
CHEBBI Talel <tchebbi.ext@MacBook-Pro.local>
2026-01-19 21:41:43 +0100

    Instantiate iam basic role dev

---
+++ infra/env/dev/outputs.tf
@@ -0,0 +1,11 @@
+output "lambda_api_role_arn" {
+  value = module.iam.lambda_api_role_arn
+}
+
+output "lambda_events_role_arn" {
+  value = module.iam.lambda_events_role_arn
+}
+
+output "lambda_step_functions_role_arn" {
+  value = module.iam.lambda_step_functions_role_arn
+}
---
+++ infra/env/dev/terraform.tfvars
@@ -0,0 +1,2 @@
+project_name = "spotify-dev"
+aws_region   = "eu-west-1"
