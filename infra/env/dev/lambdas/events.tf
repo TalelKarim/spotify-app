780bbc4da76bdc8cffdca3378eded3780fd1ac1a
CHEBBI Talel <tchebbi.ext@MacBook-Pro.local>
2026-01-27 21:24:00 +0100

    Instanciaton lambda events

---
+++ infra/env/dev/lambdas/events.tf
@@ -0,0 +1,34 @@
+locals {
+  event_lambdas = {
+    event_store_listening_event = {
+      role = module.iam.lambda_events_role_arn
+      env = {
+        LISTENING_EVENTS_TABLE = module.dynamodb.listening_events_table_name
+      }
+    }
+
+    event_update_track_stats = {
+      role = module.iam.lambda_events_role_arn
+      env = {
+        TRACKS_TABLE = module.dynamodb.tracks_table_name
+      }
+    }
+
+    event_publish_notifications = {
+      role = module.iam.lambda_events_role_arn
+      env  = {}
+    }
+  }
+}
+
+module "event_lambdas" {
+  source   = "../../../modules/lambda"
+  for_each = local.event_lambdas
+
+  function_name = "spotify-dev-${each.key}"
+  role_arn      = each.value.role
+  handler       = "handler.main"
+  package_path  = "../../../app/lambdas/${each.key}.zip"
+
+  environment_variables = each.value.env
+}
