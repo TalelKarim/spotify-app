c7d2c2a1d81ea3605eecfa5c7fbced73d72c4db1
CHEBBI Talel <tchebbi.ext@MacBook-Pro.local>
2026-02-02 17:45:19 +0100

    Bind sqs to lambda

--- infra/env/dev/sqs_bindings.tf
+++ infra/env/dev/sqs_bindings.tf
@@ -5,3 +5,11 @@ resource "aws_cloudwatch_event_target" "eventbridge_to_sqs" {
   event_bus_name = module.eventbridge.bus_name
   arn            = module.listening_events_queue.queue_arn
 }
+
+
+
+resource "aws_lambda_event_source_mapping" "sqs_to_event_lambda" {
+  event_source_arn = module.listening_events_queue.queue_arn
+  function_name    = module.event_lambdas["event_store_listening_event"].lambda_arn
+  batch_size       = 10
+}
