a82e7e1475cb6eddc59e24251211420952aebb55
CHEBBI Talel <tchebbi.ext@macbook-pro.siege.la.priv>
2026-02-03 20:17:33 +0100

    Instantiate step functions module dev

---
+++ infra/env/dev/step_functions.tf
@@ -0,0 +1,22 @@
+module "listening_analytics" {
+  source = "../../modules/step_functions"
+
+  name      = "spotify-dev-listening-analytics"
+  role_arn = module.iam.lambda_step_functions_role_arn
+
+  definition = jsonencode({
+    StartAt = "StoreEvent"
+    States = {
+      StoreEvent = {
+        Type     = "Task"
+        Resource = module.event_lambdas["event_store_listening_event"].lambda_arn
+        Next     = "UpdateStats"
+      }
+      UpdateStats = {
+        Type     = "Task"
+        Resource = module.event_lambdas["event_update_track_stats"].lambda_arn
+        End      = true
+      }
+    }
+  })
+}
