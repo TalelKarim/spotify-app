20e0903c44e6327bdcc065d4b564c48f65245985
CHEBBI Talel <tchebbi.ext@macbook-pro.siege.la.priv>
2026-02-03 20:16:19 +0100

    Refactor eventbridge instanciation

--- infra/env/dev/eventbridge.tf
+++ infra/env/dev/eventbridge.tf
@@ -1,8 +1,13 @@
-module "eventbridge" {
-  source = "../../modules/eventbridge"
+module "eventbridge_bus" {
+  source   = "../../modules/eventbridge_bus"
+  bus_name = "spotify-dev-bus"
+}
 
-  bus_name  = "spotify-dev-bus"
-  rule_name = "spotify-dev-track-played"
+module "rule_track_played_to_sqs" {
+  source = "../../modules/eventbridge_rule"
+
+  bus_name  = module.eventbridge_bus.name
+  rule_name = "spotify-dev-track-played-to-sqs"
 
   event_pattern = jsonencode({
     source      = ["spotify.api"]
