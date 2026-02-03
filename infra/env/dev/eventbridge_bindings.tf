69ffb5468bc056d3dc66a60a785423fd4e550af4
CHEBBI Talel <tchebbi.ext@macbook-pro.siege.la.priv>
2026-02-03 20:38:07 +0100

    Refactoring

--- infra/env/dev/eventbridge_bindings.tf
+++ infra/env/dev/eventbridge_bindings.tf
@@ -1,14 +1,14 @@
 
 # sqs link to eventbridge
 resource "aws_cloudwatch_event_target" "eventbridge_to_sqs" {
-  rule           = module.eventbridge.rule_name
-  event_bus_name = module.eventbridge.bus_name
+  rule           = module.rule_track_played_to_sqs.rule_name
+  event_bus_name = module.eventbridge_bus.bus_name
   arn            = module.listening_events_queue.queue_arn
 }
 
 resource "aws_cloudwatch_event_target" "to_stepfn" {
   rule           = module.track_played_to_stepfn.rule_name
   event_bus_name = module.eventbridge_bus.name
-  arn            = module.analytics_state_machine.arn
+  arn            = module.listening_analytics.state_machine_arn
   role_arn       = module.iam.eventbridge_stepfn_role_arn
 }
--- infra/modules/eventbridge_rule/outputs.tf
+++ infra/modules/eventbridge_rule/outputs.tf
@@ -1,4 +1,4 @@
-output "bus_name" {
-  value = aws_cloudwatch_event_bus.this.name
+output "rule_name" {
+  value = aws_cloudwatch_event_rule.this.name
 }
 
