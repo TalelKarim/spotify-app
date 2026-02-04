e4f5ab0cff442d13d61a8b77d37800df9a9faa68
CHEBBI Talel <tchebbi.ext@MacBook-Pro.local>
2026-02-04 19:40:14 +0100

    Create api gateway module

---
+++ infra/modules/api-gateway/outputs.tf
@@ -0,0 +1,11 @@
+output "id" {
+  value = aws_api_gateway_rest_api.this.id
+}
+
+output "root_resource_id" {
+  value = aws_api_gateway_rest_api.this.root_resource_id
+}
+
+output "execution_arn" {
+  value = aws_api_gateway_rest_api.this.execution_arn
+}
