a832bca8debdbb2ad7d7728eb03cd05ac9ecbb96
CHEBBI Talel <tchebbi.ext@macbook-pro.siege.la.priv>
2026-02-06 18:23:37 +0100

    Update outputs dynamodb

--- infra/modules/dynamodb/outputs.tf
+++ infra/modules/dynamodb/outputs.tf
@@ -2,10 +2,35 @@ output "tracks_table_name" {
   value = aws_dynamodb_table.tracks.name
 }
 
+output "tracks_table_arn" {
+  value = aws_dynamodb_table.tracks.arn
+}
+
 output "users_table_name" {
   value = aws_dynamodb_table.users.name
 }
 
+
+output "users_table_arn" {
+  value = aws_dynamodb_table.users.arn
+}
+
+
 output "listening_events_table_name" {
   value = aws_dynamodb_table.listening_events.name
+}
+
+
+output "listening_events_table_arn" {
+  value = aws_dynamodb_table.listening_events.arn
+}
+
+
+
+output "analytics_table_name" {
+  value = aws_dynamodb_table.analytics.name
+}
+
+output "analytics_table_arn" {
+  value = aws_dynamodb_table.analytics.arn
 }
