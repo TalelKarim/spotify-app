f18ab840ffdf639be0012237c59669b7e5e1a02a
CHEBBI Talel <tchebbi.ext@macbook-pro.siege.la.priv>
2026-02-06 18:24:47 +0100

    Update iam lambdas with dynamodb access

--- infra/env/dev/iam.tf
+++ infra/env/dev/iam.tf
@@ -11,4 +11,21 @@ module "iam" {
   sqs_queue_arn = module.listening_events_queue.queue_arn
   orchestration_lambda_arns = local.orchestration_lambda_arns
 
+  dynamodb_table_arns = {
+    api = [
+      module.dynamodb.tracks_table_arn,
+      module.dynamodb.users_table_arn
+    ]
+
+    events = [
+      module.dynamodb.listening_events_table_arn,
+      module.dynamodb.tracks_table_arn
+    ]
+
+    orch = [
+      module.dynamodb.tracks_table_arn,
+      module.dynamodb.users_table_arn,
+      module.dynamodb.analytics_table_arn
+    ]
+  }
 }
