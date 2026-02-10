cc49d3f484ff1266f14ad48028cc266118ea9127
CHEBBI Talel <tchebbi.ext@macbook-pro.siege.la.priv>
2026-02-10 18:33:28 +0100

    Code lambda event store listening event

--- app/lambdas/events/store_listening_event/handler.py
+++ app/lambdas/events/store_listening_event/handler.py
@@ -3,12 +3,40 @@ import os
 import boto3
 from datetime import datetime
 
+dynamodb = boto3.resource("dynamodb")
 
+TABLE_NAME = os.environ["LISTENING_EVENTS_TABLE"]
+table = dynamodb.Table(TABLE_NAME)
 
-def handler(event, context):
+def main(event, context):
+    """
+    Event attendu = Detail EventBridge
+    """
+
+    # EventBridge enveloppe
+    detail = event.get("detail", {})
+
+    track_id = detail["trackId"]
+    user_id = detail["userId"]
+    timestamp = detail["timestamp"]
+
+    pk = f"TRACK#{track_id}"
+    sk = f"TS#{timestamp}"
+
+    item = {
+        "PK": pk,
+        "SK": sk,
+        "userId": user_id,
+        "eventType": detail["eventType"],
+        "source": detail["source"],
+        "metadata": detail.get("metadata", {}),
+        "createdAt": datetime.utcnow().isoformat() + "Z"
+    }
+
+    table.put_item(Item=item)
+
     return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "Hello World !",
-        })
+        "status": "OK",
+        "pk": pk,
+        "sk": sk
     }
