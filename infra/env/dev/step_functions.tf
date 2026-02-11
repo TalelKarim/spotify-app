45990a6ebd44403169905b44c781bce7fc60866a
CHEBBI Talel <tchebbi.ext@MacBook-Pro.local>
2026-02-11 18:06:24 +0100

    Update state machine definition

--- infra/env/dev/step_functions.tf
+++ infra/env/dev/step_functions.tf
@@ -7,24 +7,27 @@ module "listening_analytics" {
   definition = jsonencode({
     StartAt = "UpdateTrackStats"
     States = {
-
       UpdateTrackStats = {
         Type     = "Task"
         Resource = module.orchestration_lambdas["orch_update_track_stats"].lambda_arn
+        ResultPath = "$.trackStatsResult"
         Next     = "UpdateUserStats"
       }
 
       UpdateUserStats = {
         Type     = "Task"
         Resource = module.orchestration_lambdas["orch_update_user_stats"].lambda_arn
+        ResultPath = "$.userStatsResult"
         Next     = "ComputeAnalytics"
       }
 
       ComputeAnalytics = {
         Type     = "Task"
         Resource = module.orchestration_lambdas["orch_compute_analytics"].lambda_arn
+        ResultPath = "$.analyticsResult"
         End      = true
       }
     }
   })
+
 }
