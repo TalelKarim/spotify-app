c38016529412a9695481ce89138c9857292ce0ab
CHEBBI Talel <tchebbi.ext@macbook-pro.siege.la.priv>
2026-02-13 18:12:04 +0100

    Create lambda api get users code

--- app/lambdas/api/get_user/handler.py
+++ app/lambdas/api/get_user/handler.py
@@ -1,14 +1,46 @@
-import json
 import os
 import boto3
-from datetime import datetime
+import json
+from decimal import Decimal
+
+dynamodb = boto3.resource("dynamodb")
+TABLE_NAME = os.environ["USERS_TABLE"]
+table = dynamodb.Table(TABLE_NAME)
 
 
+def decimal_to_int(value):
+    if isinstance(value, Decimal):
+        return int(value)
+    return value
 
-def handler(event, context):
+
+def main(event, context):
+
+    user_id = event["pathParameters"]["userId"]
+
+    pk = f"USER#{user_id}"
+    sk = "METADATA"
+
+    response = table.get_item(
+        Key={
+            "PK": pk,
+            "SK": sk
+        }
+    )
+
+    item = response.get("Item")
+
+    if not item:
+        return {
+            "statusCode": 404,
+            "body": json.dumps({"error": "User not found"})
+        }
+
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "Hello World !",
+            "userId": user_id,
+            "totalPlays": decimal_to_int(item.get("totalPlays", 0)),
+            "lastPlayedTrack": item.get("lastPlayedTrack")
         })
     }
