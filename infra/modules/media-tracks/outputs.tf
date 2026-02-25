feb1e1b156a400ad5c5b6d83e63da7759f8c5c5b
CHEBBI Talel <tchebbi.ext@macbook-pro.siege.la.priv>
2026-02-25 21:26:43 +0100

    Fix: Add missing output

--- infra/modules/media-tracks/outputs.tf
+++ infra/modules/media-tracks/outputs.tf
@@ -2,6 +2,10 @@ output "bucket_name" {
   value = aws_s3_bucket.tracks.bucket
 }
 
+
+output "bucket_arn" {
+  value = aws_s3_bucket.tracks.arn
+}
 output "cloudfront_domain" {
   value = aws_cloudfront_distribution.tracks.domain_name
 }
