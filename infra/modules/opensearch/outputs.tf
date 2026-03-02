ab3721c34bc84ab377d01a32363c80f4630fecc4
CHEBBI Talel <tchebbi.ext@macbook-pro.siege.la.priv>
2026-03-02 20:42:37 +0100

    Make opensearch cluster public

--- infra/modules/opensearch/outputs.tf
+++ infra/modules/opensearch/outputs.tf
@@ -7,8 +7,3 @@ output "domain_endpoint" {
   description = "Endpoint HTTPS du domaine OpenSearch"
   value       = aws_opensearch_domain.this.endpoint
 }
-
-output "security_group_id" {
-  description = "Security Group ID associé au domaine OpenSearch"
-  value       = aws_security_group.opensearch.id
-}
