d16db4827f0e195d05ff6ab98ab81f683c1cd9a2
CHEBBI Talel <tchebbi.ext@macbook-pro.siege.la.priv>
2026-03-10 14:29:56 +0100

    Update cognito callback urls

--- infra/env/dev/cognito.tf
+++ infra/env/dev/cognito.tf
@@ -2,7 +2,7 @@ module "cognito" {
   source                = "../../modules/cognito"
   env                   = var.env
   region                = var.aws_region
-  callback_urls         = ["http://localhost:5173"]
-  logout_urls           = ["http://localhost:5173"]
+  callback_urls         = ["http://localhost:5173","http://localhost:5173/"]
+  logout_urls           = ["http://localhost:5173","http://localhost:5173/"]
   cognito_domain_prefix = "spotify-${var.env}"
 }
