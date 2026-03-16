6742106b0951521b3314e6b9c947339ca9aa1737
CHEBBI Talel <tchebbi.ext@MacBook-Pro.local>
2026-03-16 21:01:56 +0100

    Remove unecessary outputs

--- infra/env/dev/outputs.tf
+++ infra/env/dev/outputs.tf
@@ -1,15 +1,3 @@
-output "lambda_api_role_arn" {
-  value = module.iam.lambda_api_role_arn
-}
-
-output "lambda_events_role_arn" {
-  value = module.iam.lambda_events_role_arn
-}
-
-
-output "lambda_step_functions_role_arn" {
-  value = module.iam.lambda_step_functions_role_arn
-}
 
 
 output "kms_key_arn" {
