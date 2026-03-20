6d9df297de4cda54dd9eb71d10db75f18349a262
CHEBBI Talel <tchebbi.ext@macbook-pro.siege.la.priv>
2026-03-20 18:42:23 +0100

    Fix: wrong modules reference in observability module

--- infra/env/dev/observability.tf
+++ infra/env/dev/observability.tf
@@ -12,12 +12,12 @@ module "observability_core" {
   sqs_queue_name        = var.sqs_queue_name
   dynamodb_table_names  = var.dynamodb_table_names
 
-  frontend_distribution_id             = module.frontend_web.cloudfront_distribution_id
-  media_distribution_id                = module.media_tracks.cloudfront_distribution_id
-  cloudfront_function_name             = module.frontend_web.cloudfront_function_name
+  frontend_distribution_id             = module.frontend.cloudfront_distribution_id
+  media_distribution_id                = module.media.cloudfront_distribution_id
+  cloudfront_function_name             = module.frontend.cloudfront_function_name
   enable_cloudfront_additional_metrics = var.enable_cloudfront_additional_metrics
 
-  state_machine_arn      = module.step_functions.state_machine_arn
+  state_machine_arn      = module.listening_analytics.state_machine_arn
   event_bus_name         = module.eventbridge_bus.bus_name
   eventbridge_rule_names = [module.rule_track_played_to_sqs.rule_name, module.rule_track_played_to_stepfn.rule_name]
   opensearch_domain_name = module.opensearch.domain_name
@@ -37,9 +37,9 @@ module "monitoring_api" {
   api_stage_name   = var.api_stage_name
   alarm_topic_arn  = module.observability_core.sns_topic_arn
 
-  frontend_distribution_id             = module.frontend_web.distribution_id
-  media_distribution_id                = module.media_tracks.distribution_id
-  cloudfront_function_name             = module.frontend_web.cloudfront_function_name
+  frontend_distribution_id             = module.frontend.distribution_id
+  media_distribution_id                = module.media.distribution_id
+  cloudfront_function_name             = module.frontend.cloudfront_function_name
   enable_cloudfront_additional_metrics = var.enable_cloudfront_additional_metrics
 }
 
@@ -53,10 +53,10 @@ module "monitoring_compute_data" {
   dynamodb_table_names  = var.dynamodb_table_names
   alarm_topic_arn       = module.observability_core.sns_topic_arn
 
-  dlq_queue_name                        = module.sqs.dlq_queue_name
+  dlq_queue_name                        = module.listening_events_queue.dlq_queue_name
   state_machine_arn                     = module.step_functions.state_machine_arn
-  event_bus_name                        = module.eventbridge.bus_name
-  eventbridge_rule_names                = module.eventbridge.rule_names
+  event_bus_name                        = module.eventbridge_bus.bus_name
+  eventbridge_rule_names                = [module.rule_track_played_to_sqs.rule_name, module.rule_track_played_to_stepfn.rule_name]
   opensearch_domain_name                = module.opensearch.domain_name
   opensearch_free_storage_threshold_mib = var.opensearch_free_storage_threshold_mib
 }
