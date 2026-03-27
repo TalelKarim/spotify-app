9723c3d0c624d93b275490ef8da55a28083a031f
CHEBBI Talel <tchebbi.ext@macbook-pro.siege.la.priv>
2026-03-27 18:30:02 +0100

    Fix: remove duplicate state machine role declaration

--- infra/modules/iam/lambda.tf
+++ infra/modules/iam/lambda.tf
@@ -2,20 +2,6 @@
 # IAM Roles
 ############################
 
-resource "aws_iam_role" "step_functions" {
-  name = "${var.project_name}-step-functions-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "states.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
 
 
 
