1c0ca2bc5d48d085511814b90aa762e391b8b4f9
CHEBBI Talel <tchebbi.ext@macbook-pro.siege.la.priv>
2026-04-15 15:21:18 +0200

    Deploy front

--- infra/env/dev/eventbridge.tf
+++ infra/env/dev/eventbridge.tf
@@ -3,6 +3,7 @@ module "eventbridge_bus" {
   bus_name = "spotify-dev-bus"
 }
 
+
 module "rule_track_played_to_sqs" {
   source = "../../modules/eventbridge_rule"
 
@@ -14,7 +15,6 @@ module "rule_track_played_to_sqs" {
     detail-type = ["TrackPlayed"]
   })
 }
-
 
 module "rule_track_played_to_stepfn" {
   source = "../../modules/eventbridge_rule"
